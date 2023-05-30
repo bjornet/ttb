ef9872bc4ad6b8e74d9b7869e84f2832c8aecf8d
Viktor Rizk <viktor.rizk@osynlig.se>
2023-05-30 10:10:39 +0200

    added som spinners to branch command

--- src/commands/branch.ts
+++ src/commands/branch.ts
@@ -1,15 +1,39 @@
+import ora from "ora";
 import { getBranchName } from "../branchName.js";
 import { getGitCheckoutBranchCommand } from "../git.js";
 
 export const branch = async (arg: string, options: any) => {
+  const spinner = ora("Creating new branch from ticket ID.").start();
+
   /**
    * @todo validate args
    */
   const issueId = Number(arg);
 
+  spinner.start("Creating branch name.");
+
   const branchName = await getBranchName(issueId);
+
+  if (!branchName) {
+    spinner.fail("Branch name could not be created.");
+    return;
+  }
+
+  spinner.succeed("Branch name created.");
+
+  spinner.start("Checking out new branch.");
 
   const gitCheckoutBranchCommand = getGitCheckoutBranchCommand(branchName);
 
-  console.log("gitCheckoutBranchCommand", gitCheckoutBranchCommand);
+  if (!gitCheckoutBranchCommand) {
+    spinner.fail("Branch could not be checked out.");
+    return;
+  }
+
+  spinner.succeed("Branch checked out.");
+
+  console.log(
+    "gitCheckoutBranchCommand should check run this command: ",
+    gitCheckoutBranchCommand
+  );
 };
