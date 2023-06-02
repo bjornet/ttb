d70d9a984dbc5fd304bec9c137ed2d9d28193c0f
Viktor Rizk <viktor.rizk@osynlig.se>
2023-06-02 10:31:12 +0200

    removed legacy files

--- git/mapOverType.js
+++
@@ -1,7 +0,0 @@
-import { gitJiraTypeMap } from "../jira/index.js";
-
-export const mapOverType = (jiraType) =>
-  gitJiraTypeMap.reduce(
-    (acc, curr) => (jiraType && jiraType === curr.jira ? curr.git : acc),
-    null
-  );
--- jira/index.js
+++
@@ -1,3 +0,0 @@
-export { setupJiraConnector, fetchJiraIssue } from './fetchJiraIssue.js';
-export { gitJiraTypeMap } from './gitJiraTypeMap.js';
-export { isJiraCodeValid, getExtractJiraCode } from './jiraCodeManager.js';
