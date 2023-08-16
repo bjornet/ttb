52ddfe022a07aa4af460f856d38327e76a7cfe0a
Rizken <viktor.rizk@osynlig.se>
2023-08-16 16:46:50 +0200

    install eslint and fix problems

--- src/modules/commands/branch.ts
+++ src/modules/commands/branch.ts
@@ -1,20 +1,21 @@
-import ora from "ora";
-import { getBranchName } from "../git/branchName.js";
-import { createLinkedBranch } from "../api/github/createLinkedBranch.js";
-import { getHeadRef } from "../git/getHeadRef.js";
-import { gitPull } from "../git/gitPull.js";
-import { checkoutBranch } from "../git/checkoutBranch.js";
-import { createLocalBranch } from "../git/createLocalBranch.js";
-import { checkIfBranchExists } from "../git/checkIfBranchExists.js";
-import { getConfig } from "../config/getConfig.js";
+import ora from 'ora';
+import { getBranchName } from '../git/branchName.js';
+import { createLinkedBranch } from '../api/github/createLinkedBranch.js';
+import { getHeadRef } from '../git/getHeadRef.js';
+import { gitPull } from '../git/gitPull.js';
+import { checkoutBranch } from '../git/checkoutBranch.js';
+import { createLocalBranch } from '../git/createLocalBranch.js';
+import { checkIfBranchExists } from '../git/checkIfBranchExists.js';
+import { getConfig } from '../config/getConfig.js';
+import { OptionValues } from 'commander';
 
-export const branch = async (arg: string, options: any) => {
-  const spinner = ora("Creating new branch from ticket ID.").start();
+export const branch = async (arg: string, options: OptionValues) => {
+  const spinner = ora('Creating new branch from ticket ID.').start();
 
   const { activeCredential } = await getConfig();
 
   if (!activeCredential) {
-    spinner.fail("No active credential.");
+    spinner.fail('No active credential.');
     return;
   }
 
@@ -23,25 +24,25 @@ export const branch = async (arg: string, options: any) => {
    */
   const issueNumber = Number(arg);
 
-  spinner.start("Creating branch name.");
+  spinner.start('Creating branch name.');
 
   const branch = await getBranchName(issueNumber, options.type);
 
   if (!branch) {
-    spinner.fail("Branch name could not be created.");
+    spinner.fail('Branch name could not be created.');
     return;
   }
 
   const { branchName, issueId } = branch;
 
-  spinner.succeed("Branch name created.");
+  spinner.succeed('Branch name created.');
 
-  spinner.start("Get head branch.");
+  spinner.start('Get head branch.');
 
   const headBranch = await getHeadRef();
 
   if (!headBranch) {
-    spinner.fail("Head branch could not be retrieved.");
+    spinner.fail('Head branch could not be retrieved.');
     return;
   }
 
@@ -52,25 +53,21 @@ export const branch = async (arg: string, options: any) => {
   );
 
   if (branchExists) {
-    spinner.fail("Branch already exists.");
+    spinner.fail('Branch already exists.');
     return;
   }
 
-  const linkedBranch = await createLinkedBranch(
-    issueId,
-    headBranch.output,
-    branchName
-  );
+  const linkedBranch = await createLinkedBranch(issueId, headBranch.output, branchName);
 
   if (!linkedBranch) {
-    spinner.fail("Linked branch could not be created.");
+    spinner.fail('Linked branch could not be created.');
 
-    spinner.start("Creating local branch instead");
+    spinner.start('Creating local branch instead');
 
     const newLocalBranch = await createLocalBranch(branchName);
 
     if (!newLocalBranch) {
-      spinner.fail("Local branch could not be created");
+      spinner.fail('Local branch could not be created');
       return;
     }
 
@@ -83,14 +80,14 @@ export const branch = async (arg: string, options: any) => {
   const gitPulled = await gitPull();
 
   if (!gitPulled) {
-    spinner.fail("Git pull failed.");
+    spinner.fail('Git pull failed.');
     return;
   }
 
   const branchCheckedOut = await checkoutBranch(linkedBranch);
 
   if (!branchCheckedOut) {
-    spinner.fail("Branch could not be checked out.");
+    spinner.fail('Branch could not be checked out.');
   }
 
   spinner.succeed(`Checked out to: ${branchName}`);
--- src/modules/config/removeCredential.ts
+++ src/modules/config/removeCredential.ts
@@ -1,10 +1,7 @@
-import { Credential } from "../../types/types.js";
-import { getConfig } from "./getConfig.js";
-import { updateFile } from "../../utils/updateFile.js";
+import { getConfig } from './getConfig.js';
+import { updateFile } from '../../utils/updateFile.js';
 
-export const removeCredential = async (
-  credential: string
-): Promise<boolean> => {
+export const removeCredential = async (credential: string): Promise<boolean> => {
   const { credentials, credentialsPath } = await getConfig();
   if (!credentials) {
     return false;
